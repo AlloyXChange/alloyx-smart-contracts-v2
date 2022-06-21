dc751baad46e2b063ec9d43b4e6e2eb6dc2c1461
Zhang Ruiyang <zhang.ruiyang@toptal.com>
2022-06-21 01:07:20 -0400

    change deployment scripts

---
+++ scripts/deploy/kovan/08_Deploy_StakeInfo.js
@@ -0,0 +1,33 @@
+const { getNamedAccounts, deployments, network, run } = require("hardhat")
+const {
+  networkConfig,
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config")
+const { verify } = require("../helper-functions")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log, get } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+  // Default one below is ETH/USD contract on Kovan
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS
+  log("----------------------------------------------------")
+  const alloy = await deploy("AlloyxStakeInfo", {
+    from: deployer,
+    args: ["0x0000000000000000000000000000000000000000"],
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  })
+
+  // Verify the deployment
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...")
+    await verify(alloy.address, ["0x0000000000000000000000000000000000000000"])
+  }
+}
+
+module.exports.tags = ["all", "feed", "main"]
